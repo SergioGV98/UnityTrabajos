9288619e7667a0d0326956260b0246df269817a4
Sergio García Vico <sergiogarciavicooficial@gmail.com>
2024-01-25 13:25:05 +0100

    a

--- P4.4. Raycasting. Garcia Vico, Sergio/Assets/Scripts/EnemigoIA.cs
+++ P4.4. Raycasting. Garcia Vico, Sergio/Assets/Scripts/EnemigoIA.cs
@@ -12,14 +12,54 @@ public class EnemigoIA : MonoBehaviour
 
     private EstadoEnemigo estado = EstadoEnemigo.Parado;
 
+    public float angMinimo = -30f;
+    public float angMaximo = 30f;
+    public float vAngular = 150f;
+    public int direccion = 1;
+
+    private float anguloActual = 0f;
+    Vector3 originalPos;
+
     void Start()
     {
-        
+        if (direccion == 0)
+        {
+            direccion = -1;
+        }
+        originalPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
     }
 
     void Update()
     {
-        
+        if (estado == EstadoEnemigo.Andando)
+        {
+            StartAnimation();
+        }
+        else
+        {
+            StopAnimation();
+        }
+    }
+
+    public void StartAnimation()
+    {
+
+        anguloActual += vAngular * direccion * Time.deltaTime;
+
+        if (anguloActual > angMaximo || anguloActual < angMinimo)
+        {
+            direccion = -direccion;
+            anguloActual = Mathf.Clamp(anguloActual, angMinimo, angMaximo);
+        }
+
+        transform.localEulerAngles = new Vector3(anguloActual, transform.localEulerAngles.y, transform.localEulerAngles.z);
+
+    }
+
+    public void StopAnimation()
+    {
+        transform.position = originalPos;
+
     }
 
 }
